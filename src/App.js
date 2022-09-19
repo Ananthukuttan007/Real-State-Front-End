a51432995f450fecb06d7d89253c798ec7481e69
Rakeshdarda19 <rakeshdarda19@gmail.com>
2022-09-19 22:39:18 +0530

    SignIn and SignUp page created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-
+import Login from './components/login/login'
+import Signup from './components/signUp/signUp'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 function App() {
   return (
     <>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/login' element = {<Login/>} />
+        <Route path='/sign-up' element= {<Signup/>} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
---
+++ src/components/login/login.js
@@ -0,0 +1,37 @@
+import "../login/login.css"
+import {Link} from "react-router-dom"
+const Login = () => {
+    return (
+        <>
+            <div>
+                <div className="login-container">
+                    <h1>Logo</h1>
+                    <p>Enter your credentials to access your network</p>
+                    <input
+                        type="text"
+                        placeholder="&nbsp;&nbsp;User ID"
+                        name="username"
+
+                     /> <br></br>
+                     <input
+                        type="password"
+                        name="password"
+                        placeholder="&nbsp;&nbsp;Password"
+                     /> <br></br>
+                     <button type="submit">
+                        Sign In
+                     </button>  <br></br>
+                     <Link to="/sign-up">
+                     <p className="signup">
+                       Sign Up 
+                     </p>
+                     </Link>
+                </div>
+                <p className="pg">Don't have an accout? <span className="name">Sign Up</span></p>
+            </div>
+
+        </>
+    )
+}
+
+export default Login;
