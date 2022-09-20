247fcbf6d9e1053dd4d93cb34695cd00e3e163bc
Soumik Seth <soumikseth28@gmail.com>
2022-09-20 16:14:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-
-import Login from './components/login/login'
-import Signup from './components/signUp/signUp'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-=======
 import Login from "./components/login/login";
 import Signup from "./components/signUp/signUp";
-import PropertyView from "./components/PropertyView/PropertyView";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
->>>>>>> 1cb373f8a3f733e11bf0162ac4f8c85b50a32556
+
+import PropertyView from "./components/PropertyView/PropertyView";
+
 import BasicInfo from "./BasicInfo";
 import PropertyDetail from "./PropertyDetail";
 import GeneralInfo from "./GeneralInfo";
@@ -18,23 +13,15 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-<<<<<<< HEAD
-          <Route path='/login' element={<Login />} />
-          <Route path='/sign-up' element={<Signup />} />
-          <Route path='/basic-info' element={<BasicInfo />} />
-          <Route path='/property-detail' element={<PropertyDetail />} />
-          <Route path='/general-info' element={<GeneralInfo />} />
-          <Route path='/location-info' element={<LocationInfo />} />
-        </Routes>
-      </BrowserRouter>
-
-=======
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<PropertyView />}></Route>
           <Route path="/sign-up" element={<Signup />} />
+          <Route path="/basic-info" element={<BasicInfo />} />
+          <Route path="/property-detail" element={<PropertyDetail />} />
+          <Route path="/general-info" element={<GeneralInfo />} />
+          <Route path="/" element={<PropertyView />}></Route>
+          <Route path="/location-info" element={<LocationInfo />} />
         </Routes>
       </BrowserRouter>
->>>>>>> 1cb373f8a3f733e11bf0162ac4f8c85b50a32556
     </>
   );
 }
